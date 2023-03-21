3a27a18b8d6e68c1f2b7b33b2a87d72f35bee29c
Uchechukwu Ottah <usottah@gmail.com>
2023-03-21 21:01:40 +0000

    Initial commit: Tasks 0 - 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+	return Promise.all([uploadPhoto(), createUser()])
+		.then((value) => {
+			const res = value[0];
+			const user = value[1];
+			console.log(`${res.body} ${user.firstName} ${user.lastName}`);
+		})
+		.catch(() => console.log('Signup system offline'));
+}
