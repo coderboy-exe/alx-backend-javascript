424c86efad72400459e678ab12f0cc02a8e66c8e
Uchechukwu Ottah <usottah@gmail.com>
2023-03-22 14:34:37 +0000

    Updates: Eslint standards - learnt about making the linter skip specific code-blocks through comments

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,5 +1,7 @@
 export default function returnHowManyArguments(...args) {
   let num = 0;
+  /* eslint-disable no-unused-vars */
   for (const arg of args) num += 1;
+  /* eslint-disable no-unused-vars */
   return num;
 }
