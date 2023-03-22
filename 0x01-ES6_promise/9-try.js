5eef83d9b9b69e2543c72e2a8551d9e6ba99ffdb
Uchechukwu Ottah <usottah@gmail.com>
2023-03-22 13:26:22 +0000

    Eslint standards

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -4,10 +4,10 @@ export default function guardrail(mathFunction) {
   try {
     const res = mathFunction();
     queue.push(res);
-    } catch (err) {
-      queue.push(`Error: ${err.message}`);
-    } finally {
-      queue.push('Guardrail was processed');
-    }
-    return queue;
+  } catch (err) {
+    queue.push(`Error: ${err.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
 }
