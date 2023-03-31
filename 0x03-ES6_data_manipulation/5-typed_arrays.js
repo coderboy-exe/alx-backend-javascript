943235dcfd197b59b31541754144d69101bc0337
Uchechukwu Samuel Ottah <71130767+coderboy-exe@users.noreply.github.com>
2023-03-31 22:05:22 +0100

    Revert to previous implementation

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -6,7 +6,7 @@ const createInt8TypedArray = (length, position, value) => {
   const buffer = new ArrayBuffer(length);
   const int8View = new Int8Array(buffer);
 
-  int8View.set([value], position);
+  int8View[position] = value;
   return new DataView(buffer);
 };
 
